f57e2c3070783c54c2d9afa71930ee12be75abbf
LuisPJ <luisandres-perez@hotmail.es>
2023-12-15 16:50:49 -0500

    Add project championship services

---
+++ championship-services/championship-services/src/main/java/com/championship/championshipservices/ChampionshipServicesApplication.java
@@ -0,0 +1,13 @@
+package com.championship.championshipservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionshipServicesApplication.class, args);
+	}
+
+}
---
+++ championship-services/championship-services/src/test/java/com/championship/championshipservices/ChampionshipServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.championship.championshipservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
