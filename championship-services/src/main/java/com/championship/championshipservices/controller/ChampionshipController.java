dae04d41545058d3801398199ea1d14387f1c434
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-12-18 17:48:00 -0500

    documentation with swagger 3

--- championship-services/src/main/java/com/championship/championshipservices/controller/ChampionshipController.java
+++ championship-services/src/main/java/com/championship/championshipservices/controller/ChampionshipController.java
@@ -11,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.championship.championshipservices.model.Team;
 import com.championship.championshipservices.service.TeamService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Championship", description = "the Championship Api")
 @RestController
 @RequestMapping("/championship/v1")
 public class ChampionshipController {
@@ -21,7 +27,13 @@ public class ChampionshipController {
         this.teamService = teamService;
     }
 
-    @GetMapping("/teams")
+    @Operation(
+        summary = "Fetch all teams", 
+        description = "Fetches all the teams and their information from the database")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successful operation")
+    })
+    @GetMapping(value = "/teams", produces = "application/json")
     public ResponseEntity<List<Team>> getAllTeams(){
         return new ResponseEntity<List<Team>>(teamService.getTeams(), HttpStatus.OK); 
     }
