a8f9057c7396a64e33c47c4f110f3e79253a6ed3
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-12-17 22:11:02 -0500

    unit tests feature list-teams

--- championship-services/build.gradle.kts
+++ championship-services/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.2.1-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -23,7 +24,28 @@ dependencies {
   	runtimeOnly("com.mysql:mysql-connector-j")
   	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
-
+/* 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}*/
+
+tasks.test {
+	useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
 }
