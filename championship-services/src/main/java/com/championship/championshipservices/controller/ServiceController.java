de02702e0e42719fff415bc7d236a6d252c22d6f
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2023-12-21 20:41:48 -0500

    change RequestBody for RequestParam because GET Requests with body made in the frontend throw 400 Error because body is ignored in GET Requests when they are sent

--- championship-services/src/main/java/com/championship/championshipservices/controller/ServiceController.java
+++ championship-services/src/main/java/com/championship/championshipservices/controller/ServiceController.java
@@ -5,10 +5,12 @@ import com.championship.championshipservices.services.LoginServices;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 public class ServiceController {
 
@@ -17,7 +19,13 @@ public class ServiceController {
 
     @ApiOperation("Login del administrador")
     @GetMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody Administrator admin) {
+    public ResponseEntity<String> loginUser(
+        @RequestParam String email,
+        @RequestParam String password
+    ) {
+        Administrator admin = new Administrator();
+        admin.setEmail(email);
+        admin.setPassword(password);
         return championService.loginUser(admin);
     }
 }
