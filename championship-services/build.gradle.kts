784961a24f9a258fd235beb0ad9f55e0ea580215
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-01-31 20:15:53 -0500

    delete not used dependencies

--- championship-services/build.gradle.kts
+++ championship-services/build.gradle.kts
@@ -21,26 +21,16 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("mysql:mysql-connector-java:8.0.28")
-	implementation("javax.persistence:javax.persistence-api:2.2")
-	compileOnly ("org.projectlombok:lombok")
 	developmentOnly ("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor ("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.2")
 	testImplementation ("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 	testImplementation ("org.mockito:mockito-core:3.12.4")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 }
-/* 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}*/
 
 tasks.test {
 	useJUnitPlatform()
